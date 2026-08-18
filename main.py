d38224451c020245790582460f524287f5f6e04a
marwaahmed11 <marwaahmed200126@gmail.com>
2026-08-18 23:53:01 +0300

    first commit

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def list_todos():
             title=todo["title"],
             description=todo["description"],
             completed=todo["completed"],
-            created_at=todo["created_at"]
+            created_at=todo["created_at"],
             ## to fail the test, you can comment out the line below and uncomment the line above
             author=todo["author"]
         )
